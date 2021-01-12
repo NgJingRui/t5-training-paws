191a39e4b33dc1aa5e20ba641efcbc9252286e3a
James <ngjingrui@hotmail.com>
2021-01-12 18:43:30 +0800

    Update script for running paperspace expt

--- ps_train_t5_small_small.sh
+++ ps_train_t5_small_small.sh
@@ -2,7 +2,7 @@
 python --version
 pip --version
 python3 --version
-pip3 --version
 pip install --upgrade pip
 pip install -r requirements.txt
-python train_t5_small_small.py
+pip install torch==1.4.0
+python3 train_t5_small_small.py
