2510315e2b2805544970684bd2573bcb0314798f
James <ngjingrui@hotmail.com>
2021-03-21 14:57:43 +0800

    Improving the Training Process

--- train_t5_base_full_gpu1_QQP.py
+++ train_t5_base_full_gpu1_QQP.py
@@ -55,12 +55,12 @@ args_dict = dict(
     weight_decay=0.0,
     adam_epsilon=1e-8,
     warmup_steps=0,
-    train_batch_size=4,
-    eval_batch_size=4,
+    train_batch_size=6,
+    eval_batch_size=6,
     num_train_epochs=2,
     gradient_accumulation_steps=16,
     n_gpu=1,
-    # early_stop_callback=False,
+    early_stop_callback=False,
     fp_16=False,  # if you want to enable 16-bit training then install apex and set this to true
     opt_level='O1',
     # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
